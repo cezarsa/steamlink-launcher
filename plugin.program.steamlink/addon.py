8d4813f4e12549d2827ef397da10c3f6af498afa
Cezar Sa Espinola <cezarsa@gmail.com>
2019-01-07 00:08:24 -0200

    Fix shell formatting and experiment with hooks

--- plugin.program.steamlink/addon.py
+++ plugin.program.steamlink/addon.py
@@ -14,6 +14,7 @@ __version__ = "0.0.3"
 dialog = xbmcgui.Dialog()
 addon = xbmcaddon.Addon(id='plugin.program.steamlink')
 
+
 def main():
     """Main operations of this plugin."""
     if os.path.isfile("/tmp/steamlink-launcher.sh"):
@@ -22,33 +23,37 @@ def main():
         create_files()
         output = os.popen("sh /tmp/steamlink-launcher.sh").read()
     dialog.ok("Starting Steamlink", output)
-    #print output
+
 
 def create_files():
     """Creates bash files to be used for this plugin."""
     with open('/tmp/steamlink-launcher.sh', 'w') as outfile:
-        outfile.write('#!/bin/bash\n'
-                      'sudo openvt -c 7 -s -f clear\n'
-                      'sudo su osmc -c "sh /tmp/steamlink-watchdog.sh &" &\n'
-                      'sudo chown osmc:osmc /usr/bin/steamlink\n'
-                      'sudo su osmc -c "nohup openvt -c 7 -f -s steamlink >/dev/null 2>&1 &" &\n'
-                      'sudo openvt -c 7 -s -f clear\n'
-                      'sleep 0.5; sudo su -c "systemctl stop mediacenter &" &\n'
-                      'exit')
+        outfile.write('''#!/bin/bash
+sudo openvt -c 7 -s -f clear
+sudo su osmc -c "sh /tmp/steamlink-watchdog.sh &" &
+sudo chown osmc:osmc /usr/bin/steamlink
+sudo su osmc -c "nohup openvt -c 7 -f -s steamlink >/dev/null 2>&1 &" &
+sudo openvt -c 7 -s -f clear
+sleep 0.5
+sudo su -c "/home/osmc/.steamlink-start-hook &" &
+sudo su -c "systemctl stop mediacenter &" &
+exit''')
         outfile.close()
     with open('/tmp/steamlink-watchdog.sh', 'w') as outfile:
-        outfile.write('#!/bin/bash\n'
-                      'if [ "$HYPERIONFIX" = 1 ]; then if [ "$(pgrep hyperion)" ]; '
-                      'then sudo service hyperion stop; fi; fi\n'
-                      'sleep 8\n'
-                      'if [ "$HYPERIONFIX" = 1 ]; then if [ ! "$(pgrep hyperion)" ]; then '
-                      'sudo service hyperion start; fi; fi\n'
-                      'while true; do VAR1="$(pgrep steamlink)"; if [ ! "$VAR1" ]; then\n'
-                      'sudo openvt -c 7 -s -f clear; sudo su -c '
-                      '"sudo systemctl restart mediacenter &" &\n'
-                      'exit\n'
-                      'else sleep 2; fi\n'
-                      'done\n'
-                      'exit')
+        outfile.write('''#!/bin/bash
+sleep 8
+while true; do 
+    VAR1="$(pgrep steamlink)"
+    if [ ! "$VAR1" ]; then
+        sudo openvt -c 7 -s -f clear
+        sudo su -c "/home/osmc/.steamlink-stop-hook &" &
+        sudo su -c "sudo systemctl restart mediacenter &" &
+        exit
+    else 
+        sleep 2
+    fi
+done
+exit''')
         outfile.close()
+
 main()
